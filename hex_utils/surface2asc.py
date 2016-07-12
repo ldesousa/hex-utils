a879e6105a062575fce363e1793e86b24a183d0b
Luís de Sousa <luis.de.sousa@protonmail.ch>
2016-07-12 15:55:44 +0200

    Surface2asc now importing surface function dynamically.

--- hex_utils/surface2asc.py
+++ hex_utils/surface2asc.py
@@ -3,25 +3,24 @@
 #
 # Copyright (c) 2016 - Luís Moreira de Sousa
 #
-# Creates a rectangular ESRI ASCII grids by sampling a a given surface.
+# Creates a rectangular ESRI ASCII grid by sampling a a given surface.
 #
 # Author: Luís Moreira de Sousa (luis.de.sousa[@]protonmail.ch)
 # Date: 03-06-2016 
-#
-# [0] https://github.com/ldesousa/HexAsciiBNF
 
 import math
 from hex_utils.asc import ASC
-from surfaceSimple import fun
 
 # ----- Main ----- #
 def main():
 
 	x_start = 0
 	y_start = 0
-	x_end = 2001
-	y_end = 2001
+	x_end = 2000
+	y_end = 2000
 	size = 20
+	moduleName = 'surfaceSimple'
+	functionName = 'fun'
 	
 	grid = ASC()
 	grid.init(	
@@ -31,18 +30,17 @@ def main():
 		y_start,
 		size, "")
 	
-	print("(0,0): " + str(fun(0,0)))	
-	print("(0,2000): " + str(fun(0,2000)))
-	print("(2000,0): " + str(fun(2000,0)))
-	print("(2000,2000): " + str(fun(2000,2000)))
-	
 	x_start += size / 2
 	y_start += size / 2
+	
+	# Dynamically import surface function
+	module = __import__(moduleName, globals(), locals(), [functionName])
+	function = getattr(module, functionName)
 	
 	for i in range(grid.ncols):
 		for j in range(grid.nrows):
 			grid.set(i, grid.nrows - j - 1, 
-				fun(x_start + i * size, y_start + j * size))
+				function(x_start + i * size, y_start + j * size))
 		
 	grid.save("temp.asc")
 	

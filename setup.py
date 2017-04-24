097f7fde319123952b4a94dd73fb74894dd37d09
Luís de Sousa <luis.de.sousa@protonmail.ch>
2017-04-24 15:06:40 +0200

    Setup for release 0.3.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "hex-utils",
-    version = "0.2.2.3",
+    version = "0.3",
     packages = find_packages(),#['utils'],
     install_requires=[
         'cycler',
